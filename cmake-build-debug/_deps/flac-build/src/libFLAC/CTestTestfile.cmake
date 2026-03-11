903c9243d7047d4c7f7aed014596d39bf1d1da3a
WiktorM8 <wiktor.muller8@gmail.com>
2026-03-11 22:19:06 +0100

    Remove cmake-build-debug

--- cmake-build-debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/pliki/studia/semestr 4/Rozporszone/Projekt/Shipers/cmake-build-debug/_deps/flac-src/src/libFLAC
-# Build directory: D:/pliki/studia/semestr 4/Rozporszone/Projekt/Shipers/cmake-build-debug/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
